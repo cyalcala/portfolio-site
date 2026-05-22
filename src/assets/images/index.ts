02a28b8283508f46b3f37a93b205810609ddfba6
Cyrus Alcala <cyrusalcala.agency@gmail.com>
2026-05-22 21:23:31 +0800

    feat(design): swap profile images and update theme colors to match the pink hoodie image

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,5 @@
-import profile from './profile.webp';
-import profile2 from './profile2.webp';
+import profile from './profile.png';
+import profile2 from './profile2.jpg';
 import esmail from './esmail.webp';
 import mohammad from './mohammad.webp';
 import workBg1 from './1.webp';
